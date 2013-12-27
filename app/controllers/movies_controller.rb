d9596b72c4068b60f16086c303ea617d317c1449
Adam89 <Adamm1@aston.ac.uk>
2013-12-26 22:17:35 -0700

    added validations to view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,33 +6,39 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
-  
+
   def edit
     @movie = Movie.find(params[:id])
   end
-  
+
   def update
     @movie = Movie.find(params[:id])
-    @movie.update(movie_params)
-    redirect_to @movie
+    if @movie.update(movie_params)
+      redirect_to @movie
+    else
+      render :edit
+    end
   end
 
   def new
     @movie = Movie.new
   end
-  
+
   def create
     @movie = Movie.new(movie_params)
-    @movie.save
-    redirect_to @movie
+    if @movie.save
+      redirect_to @movie
+    else
+      render :new
+    end
   end
-  
+
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
     redirect_to movies_url
   end
-  
+
 private
 
   def movie_params
