f58e177906874543eb1eea2420a5e25609284407
Adam89 <Adamm1@aston.ac.uk>
2014-01-01 15:00:35 -0700

    spent 1 hour debugging a simple typo in my damn params that I permitted to go through to my db

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,4 +4,24 @@ class ReviewsController < ApplicationController
     @movie = Movie.find(params[:movie_id])
     @reviews = @movie.reviews.all
   end
+
+  def new
+    @movie = Movie.find(params[:movie_id])
+    @review = @movie.reviews.new
+  end
+
+  def create
+    @movie = Movie.find(params[:movie_id])
+    @review = @movie.reviews.create(review_params)
+    if @review.save
+      redirect_to movie_reviews_path(@movie), notice: "Thanks for your review!"
+    else
+      render :new
+    end
+  end
+  private
+
+  def review_params
+    params.require(:review).permit(:full_name, :stars, :comment)
+  end
 end
