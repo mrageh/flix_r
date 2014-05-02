0e813120c98d3bc05d3082235f6655a1eaa43cca
Adam89 <adam@apprentice.io>
2014-05-02 16:56:17 +0200

    Without force pushing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,6 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-
       redirect_to user_path(@user), :notice => "Thanks for signing up!"
     else
       render :new
