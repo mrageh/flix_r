8bc641775e6eb0c0434dd374d9e41802414d1124
Adam89 <adam@apprentice.io>
2014-05-02 16:46:15 +0200

    ANother mistake

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to user_path(@user), :notice => 'Thanks for signing up!'
+      redirect_to user_path(@user), :notice => "Thanks for signing up!"
     else
       render :new
     end
